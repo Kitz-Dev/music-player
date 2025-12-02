871e45739a5cf99f1821e624ab27bf968bf9cc29
Romain <129210960+KitzTV@users.noreply.github.com>
2025-12-02 16:40:44 +0100

    feat(play/next/previous): done

--- assets/js/app.js
+++ assets/js/app.js
@@ -1,13 +1,72 @@
 'use strict'
-
 const currentSong = document.getElementById("current-song")
 const playButton = document.getElementById("play-button")
+const playTooltip = document.getElementById("play-tooltip")
+const nextButton = document.getElementById("next-button")
+const previousButton = document.getElementById("previous-button")
+
 currentSong.volume = 0.5
 
-playButton.addEventListener("click", () => {
-    if (!currentSong.paused) {
-        currentSong.pause()
-    } else {
-        currentSong.play()
+let sortedAlphabetically = []
+let currentIndex = 0
+
+const playSong = async () => {
+    try {
+        const res = await fetch("../../data/playlist.json")
+
+        if (!res.ok) {
+            throw new Error(`HTTP error! status: ${res.status}`)
+        }
+
+        const data = await res.json()
+        const playlist2 = data[1].songs
+
+        sortedAlphabetically = [...playlist2].sort((a, b) => {
+            if (a.title < b.title) {
+                return -1
+            }
+            if (a.title > b.title) {
+                return 1
+            }
+            return 0
+        })
+        console.log(sortedAlphabetically)
+
+        currentSong.src = sortedAlphabetically[0].url
+
+        playButton.addEventListener("click", () => {
+            if (!currentSong.paused) {
+                currentSong.pause()
+                playTooltip.textContent = "Play"
+                document.getElementById("play-button-img").setAttribute("href", "./img/sprite.svg#play-button")
+            } else {
+                currentSong.play()
+                playTooltip.textContent = "Pause"
+                document.getElementById("play-button-img").setAttribute("href", "./img/sprite.svg#pause-button")
+            }
+        })
+
+        nextButton.addEventListener("click", () => {
+            currentIndex = (currentIndex + 1) % sortedAlphabetically.length
+            currentSong.src = sortedAlphabetically[currentIndex].url
+            currentSong.play()
+            playTooltip.textContent = "Pause"
+            document.getElementById("play-button-img").setAttribute("href", "./img/sprite.svg#pause-button")
+        })
+
+        previousButton.addEventListener("click", () => {
+            currentIndex = (currentIndex - 1 + sortedAlphabetically.length) % sortedAlphabetically.length
+            currentSong.src = sortedAlphabetically[currentIndex].url
+            currentSong.play()
+            playTooltip.textContent = "Pause"
+            document.getElementById("play-button-img").setAttribute("href", "./img/sprite.svg#pause-button")
+        })
+    } catch (error) {
+        console.log(error)
     }
-})
+}
+
+playSong()
+
+
+
